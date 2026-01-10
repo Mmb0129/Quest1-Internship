f79045d3154e55956b031c957de7e5b0097ee893
Mmb0129 <michealberdinanth@gmail.com>
2026-01-10 19:29:19 +0530

    Refactored with gradle

---
+++ lisp-interpreter/app/src/main/java/com/micheal/lisp/LispInterpreterApp.java
@@ -0,0 +1,8 @@
+package com.micheal.lisp;
+
+public class LispInterpreterApp {
+
+    public static void main(String[] args) {
+        System.out.println("Lisp Interpreter starting...");
+    }
+}
---
+++ lisp-interpreter/app/src/test/java/com/micheal/lisp/LispInterpreterAppTest.java
@@ -0,0 +1,12 @@
+package com.micheal.lisp;
+
+import org.junit.Test;
+
+public class LispInterpreterAppTest {
+
+    @Test
+    public void applicationStartsSuccessfully() {
+        // For now, this test simply ensures the application class exists
+        LispInterpreterApp app = new LispInterpreterApp();
+    }
+}
