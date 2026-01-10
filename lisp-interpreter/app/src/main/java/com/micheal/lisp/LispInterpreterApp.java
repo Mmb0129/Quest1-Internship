d317d2cc1435dfbd55d20e48bca380044d2b4d96
Mmb0129 <michealberdinanth@gmail.com>
2026-01-10 21:07:58 +0530

    started with addition

--- lisp-interpreter/app/src/main/java/com/micheal/lisp/LispInterpreterApp.java
+++ lisp-interpreter/app/src/main/java/com/micheal/lisp/LispInterpreterApp.java
@@ -1,8 +1,23 @@
 package com.micheal.lisp;
+import com.micheal.lisp.ast.*;
+import com.micheal.lisp.visitor.EvaluationVisitor;
+
+import java.util.List;
 
 public class LispInterpreterApp {
 
     public static void main(String[] args) {
-        System.out.println("Lisp Interpreter starting...");
+        EvaluationVisitor evaluator = new EvaluationVisitor();
+
+        Node expression = new ListNode(
+                List.of(
+                        new SymbolNode("+"),
+                        new NumberNode(1),
+                        new NumberNode(2)
+                )
+        );
+
+        Object result = expression.accept(evaluator);
+        System.out.println(result); // should print 3
     }
 }
--- lisp-interpreter/app/src/main/java/com/micheal/lisp/visitor/EvaluationVisitor.java
+++ lisp-interpreter/app/src/main/java/com/micheal/lisp/visitor/EvaluationVisitor.java
@@ -2,12 +2,16 @@ package com.micheal.lisp.visitor;
 
 import com.micheal.lisp.ast.*;
 
+import java.util.List;
+
 public class EvaluationVisitor implements Visitor {
+
 
     @Override
     public Object visit(NumberNode node) {
-        return null;
+        return node.getValue();
     }
+
 
     @Override
     public Object visit(SymbolNode node) {
@@ -16,6 +20,48 @@ public class EvaluationVisitor implements Visitor {
 
     @Override
     public Object visit(ListNode node) {
-        return null;
+        var elements= node.getElements();
+
+        if(elements.isEmpty()){
+            throw new IllegalStateException("Cannot evaluate empty list");
+        }
+        var operatorNode= elements.get(0);
+
+        if(!(operatorNode instanceof SymbolNode)){
+            throw new IllegalArgumentException("First element must be an operator");
+
+        }
+
+        String operator = ((SymbolNode) operatorNode).getName();
+
+        switch (operator) {
+            case "+":
+                return evaluateAddition(elements.subList(1, elements.size()));
+            case "*":
+                return evaluateMultiplication(elements.subList(1, elements.size()));
+            default:
+                throw new UnsupportedOperationException("Unknown operator: " + operator);
+        }
+
     }
+
+    private int evaluateAddition(List<Node> operands) {
+        int result = 0;
+        for (Node operand : operands) {
+            Object value = operand.accept(this);
+            result += (int) value;
+        }
+        return result;
+    }
+
+    private int evaluateMultiplication(List<Node> operands) {
+        int result = 1;
+        for (Node operand : operands) {
+            Object value = operand.accept(this);
+            result *= (int) value;
+        }
+        return result;
+    }
+
+
 }
