bfec38b28cb5e7e9445656b5c504d301a74cccc7
Mmb0129 <michealberdinanth@gmail.com>
2026-01-19 13:51:22 +0530

    Feedback Implementation: Integrated PMD and SpotBugs

--- lisp-interpreter/app/src/main/java/com/micheal/lisp/exception/LispException.java
+++ lisp-interpreter/app/src/main/java/com/micheal/lisp/exception/LispException.java
@@ -3,7 +3,8 @@ package com.micheal.lisp.exception;
 // base exception class for all my lisp exceptions
 
 public class LispException extends RuntimeException {
-    
+    private static final long serialVersionUID = 1L;
+
     public LispException(String message) {
         super(message);
     }
--- lisp-interpreter/app/src/test/java/com/micheal/lisp/EvaluatorTest.java
+++ lisp-interpreter/app/src/test/java/com/micheal/lisp/EvaluatorTest.java
@@ -5,12 +5,6 @@ import com.micheal.lisp.visitor.EvaluationVisitor;
 import com.micheal.lisp.ast.Node;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
